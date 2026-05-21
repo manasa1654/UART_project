5beb99dba6526f93e178a3b4ffba64d8eec9b59f
Manasa <jmanasa@feserver.mirafra.com>
2026-05-21 11:00:54 +0530

    TX changes

--- src/u_xmit.v
+++ src/u_xmit.v
@@ -14,6 +14,7 @@ output reg xmit_active
 reg [WORD_LEN-1:0] shift_reg;
 reg [($clog2(WORD_LEN)-1):0] bit_count;
 reg [3:0] baud_count;
+reg [3:0] done_count;
 
 localparam IDLE  = 2'b00;
 localparam START = 2'b01;
@@ -21,7 +22,6 @@ localparam DATA  = 2'b10;
 localparam STOP  = 2'b11;
 
 reg [1:0] state;
-
 
 always @(posedge baud_16_clk or negedge sys_rst_l)
 begin
@@ -31,6 +31,7 @@ uart_XMIT_dataH <= 1'b1;
 xmit_doneH <= 1'b0;
 xmit_active <= 1'b0;        
 baud_count <= 0;
+done_count <= 0;
 shift_reg <= 0;
 bit_count <= 0;
 state <= IDLE;
@@ -38,69 +39,84 @@ end
 
 else
 begin
+
+if (done_count != 0)
+begin
+done_count <= done_count - 1'b1;
+xmit_doneH <= 1'b1;
+xmit_active <= 1'b1;
+end
+else
+begin
 xmit_doneH <= 1'b0;
+end
 
 case(state)
+
 IDLE:
 begin
  uart_XMIT_dataH<=1'b1;
+
+ if(done_count == 0)
  xmit_active<=1'b0;
-  if(xmitH)
-   begin
+
+ if(xmitH)
+ begin
     shift_reg <= xmit_dataH;
     bit_count<=0;
     baud_count <= 0;
     xmit_active<=1'b1;
     state <= START;
-   end
+ end
 end
 
 START:
 begin
 uart_XMIT_dataH<=1'b0;
-    baud_count <= baud_count + 1;
+baud_count <= baud_count + 1;
 
-    if(baud_count == 4'd15)
-    begin
-        baud_count <= 0;
-        state<=DATA;
-    end
+if(baud_count == 4'd15)
+begin
+    baud_count <= 0;
+    state<=DATA;
+end
 end
 
 DATA:
 begin
 uart_XMIT_dataH <= shift_reg[0];
-    baud_count <= baud_count + 1;
+baud_count <= baud_count + 1;
 
-    if(baud_count == 4'd15)
+if(baud_count == 4'd15)
+begin
+    baud_count <= 0;
+    shift_reg <= shift_reg>>1;
+
+    if(bit_count == WORD_LEN-1)
     begin
-        baud_count <= 0;
-        shift_reg <= shift_reg>>1;
-        if(bit_count == WORD_LEN-1)
-        begin
-          bit_count<= 0;
-          state <= STOP;
-        end
-     else
-       bit_count <= bit_count+1;
+      bit_count<= 0;
+      state <= STOP;
     end
+    else
+      bit_count <= bit_count+1;
+end
 end
 
 STOP:
 begin
 uart_XMIT_dataH <= 1'b1;
-    baud_count <= baud_count + 1;
+baud_count <= baud_count + 1;
 
-    if(baud_count == 4'd15)
-    begin
-        baud_count <= 0;
-        xmit_doneH <= 1'b1;
-        xmit_active <= 1'b0;
-        state <= IDLE;
-    end
+if(baud_count == 4'd15)
+begin
+    baud_count <= 0;
+    done_count <= 4'd15;
+    state <= IDLE;
 end
+end
+
 endcase
 end
 end
-endmodule
 
+endmodule
